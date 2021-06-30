1bfb2a72a0356160ea74b1a79038b7bf02cf4be5
Boye-dev <78259921+Boye-dev@users.noreply.github.com>
2021-06-30 19:55:12 +0100

    Updated

--- index.js
+++ index.js
@@ -1,69 +1,169 @@
-var d=new Date();
-var hours=d.getHours();
-var min=d.getMinutes();
-var sec=d.getSeconds();
-
-if(hours>=0&&hours<=9){
-  $(".time").html("0"+hours+" :"+min);
-  if(min>=0&&min<=9){
-    $(".time").html("0"+hours+" :0"+min);
+var d = new Date();
+var hours = d.getHours();
+var min = d.getMinutes();
+var sec = d.getSeconds();
+if (hours >= 0 && hours <= 9) {
+  $(".time").html("0" + hours + " :" + min);
+  if (min >= 0 && min <= 9) {
+    $(".time").html("0" + hours + " :0" + min);
+  } else {
+    $(".time").html("0" + hours + " :" + min);
   }
-  else{
-      $(".time").html("0"+hours+" :"+min);
-  }
-}else{
-  $(".time").html(hours+" : "+min);
+} else {
+  $(".time").html(hours + " : " + min);
 }
-$(".navbar-toggler").click(function(event, c){
-    $(event.target).addClass("disabled");
-        $("nav").toggleClass("navbar-color");
-    setTimeout(()=>{
-        $(event.target).removeClass("disabled");
-    },500);
-})
-$(".nav-link").click(function(event, c){
-    $(event.target).addClass("disabled");
-        $("nav").toggleClass("navbar-color");
-    setTimeout(()=>{
-        $(event.target).removeClass("disabled");
-    },500);
-})
-var nav = document.querySelector('nav'); // Identify target
 
-window.addEventListener('scroll', function(event) { // To listen for event
+
+if (hours >=0&&hours<12) {
+  $("video").html('<source src="header3.mp4" type="video/mp4"></source>');
+  $(".btn").removeClass("bio");
+  $(".btn").addClass("bio-morning");
+
+  var nav = document.querySelector('nav');
+  window.addEventListener('scroll', function(event) { // To listen for event
     event.preventDefault();
-
     if (window.scrollY <= 150) { // Just an example
-        nav.style.backgroundColor = ' #66000000'; // or default color
+      nav.style.backgroundColor = ' #66000000'; // or default color
     } else {
-        nav.style.backgroundColor = 'rgba(85.5, 0, 21.6, 0.99)';
-
+      nav.style.backgroundColor = '#053742';
     }
-});
+  });
 
-// if(hours>=0&&hours<12){
-//   $("video").html('<source src="header3.mp4" type="video/mp4"></source>');
-//   $(".btn").removeClass("bio");
-//   $(".btn").addClass("bio-morning");
-// $(".time-text").html("Good Morning From Boye");
-//
-//
-// }
-// else if(hours>=12&&hours<18){
-//   $("video").html('<source src="header3.mp4" type="video/mp4"></source>');
-//   $(".btn").removeClass("bio");
-//   $(".btn").addClass("bio-morning");
-//   $(".time-text").html("Good Afternoon From Boye");
-// }
-// else
- if(hours>=0&&hours<24){
-  $("video").html('<source src="night.mp4" type="video/mp4"></source>');
-  $(".video-text").css("color","white");
-  $(".talk").removeClass("bot");
-    $(".talk").addClass("bot-night");
-  $(".border-text").css({'color':'white','border-bottom': '2px solid black'});
-  $(".time-text").html("Good Evening From Boye");
+  const navLinks = document.querySelectorAll('.nav-item')
+  const menuToggle = document.getElementById('navbarSupportedContent')
+  const bsCollapse = new bootstrap.Collapse(menuToggle)
+  navLinks.forEach((l) => {
+      l.addEventListener('click', () => { bsCollapse.toggle() })
+  });
+
+$(".btn-cv").css("background-color","#053742");
+  $(".time-text").html("Good Morning From Boye");
+
+  $(".header").css("background-color","#39A2DB");
+  $("#about").css("background-color","#39A2DB");
+  $(".mor").removeClass("btn-cv");
+  $(".mor").addClass("btn-cv-morning");
+  $("strong").removeClass("name");
+  $("strong").addClass("name-morning");
+  $(".fab").removeClass("po");
+  $(".fab").addClass("po-morning");
+  $(".cor").removeClass("co");
+  $(".cor").addClass("co-morning");
+  $(".hrs").removeClass("hr-odd");
+  $(".hrs").addClass("hr-odd-morning");
+  $(".fl").removeClass("front");
+  $(".fl").addClass("front-morning");
+  $(".bm").removeClass("back-measure");
+  $(".bm").addClass("back-measure-morning");
+  $(".far").removeClass("fars");
+  $(".far").addClass("fars-morning");
+  $(".fa").removeClass("f-a");
+  $(".fa").addClass("f-a-morning");
+  $(".fas").removeClass("fs");
+  $(".fas").addClass("fs-morning");
+  $(".proj").css("background-color","#39A2DB");
+  $(".cont").css("background-color","#39A2DB");
+  $(".but").removeClass("btn-submit");
+  $(".but").addClass("btn-submit-morning");
+  $(".inp").removeClass("forms");
+  $(".inp").addClass("forms-morning");
+  $("textarea").removeClass("texta");
+  $("textarea").addClass("texta-morning");
 }
+
+else if (hours >= 12 && hours < 18) {
+  $("video").html('<source src="header3.mp4" type="video/mp4"></source>');
+  $(".btn").removeClass("bio");
+  $(".btn").addClass("bio-morning");
+  $(".time-text").html("Good Afternoon From Boye");
+
+
+
+  var nav = document.querySelector('nav');
+  window.addEventListener('scroll', function(event) { // To listen for event
+    event.preventDefault();
+    if (window.scrollY <= 150) { // Just an example
+      nav.style.backgroundColor = ' #66000000'; // or default color
+    } else {
+      nav.style.backgroundColor = '#053742';
+    }
+  });
+
+  const navLinks = document.querySelectorAll('.nav-item')
+  const menuToggle = document.getElementById('navbarSupportedContent')
+  const bsCollapse = new bootstrap.Collapse(menuToggle)
+  navLinks.forEach((l) => {
+      l.addEventListener('click', () => { bsCollapse.toggle() })
+  });
+
+$(".header").css("background-color","#39A2DB");
+$("#about").css("background-color","#39A2DB");
+$(".mor").removeClass("btn-cv");
+$(".mor").addClass("btn-cv-morning");
+$("strong").removeClass("name");
+$("strong").addClass("name-morning");
+$(".fab").removeClass("po");
+$(".fab").addClass("po-morning");
+$(".cor").removeClass("co");
+$(".cor").addClass("co-morning");
+$(".hrs").removeClass("hr-odd");
+$(".hrs").addClass("hr-odd-morning");
+$(".fl").removeClass("front");
+$(".fl").addClass("front-morning");
+$(".bm").removeClass("back-measure");
+$(".bm").addClass("back-measure-morning");
+$(".far").removeClass("fars");
+$(".far").addClass("fars-morning");
+$(".fa").removeClass("f-a");
+$(".fa").addClass("f-a-morning");
+$(".fas").removeClass("fs");
+$(".fas").addClass("fs-morning");
+$(".proj").css("background-color","#39A2DB");
+$(".cont").css("background-color","#39A2DB");
+$(".but").removeClass("btn-submit");
+$(".but").addClass("btn-submit-morning");
+$(".inp").removeClass("forms");
+$(".inp").addClass("forms-morning");
+$("textarea").removeClass("texta");
+$("textarea").addClass("texta-morning");
+}
+
+
+else if (hours >= 18 && hours < 24) {
+  $("video").html('<source src="night.mp4" type="video/mp4"></source>');
+  $(".video-text").css("color", "white");
+  $(".talk").removeClass("bot");
+  $(".talk").addClass("bot-night");
+  $(".border-text").css({
+    'color': 'white',
+    'border-bottom': '2px solid black'
+  });
+  $(".time-text").html("Good Evening From Boye");
+  $(".header").css("background-color","#555555");
+
+
+
+  const navLinks = document.querySelectorAll('.nav-item')
+  const menuToggle = document.getElementById('navbarSupportedContent')
+  const bsCollapse = new bootstrap.Collapse(menuToggle)
+  navLinks.forEach((l) => {
+      l.addEventListener('click', () => { bsCollapse.toggle() })
+  });
+
+  var nav = document.querySelector('nav');
+  window.addEventListener('scroll', function(event) { // To listen for event
+    event.preventDefault();
+    if (window.scrollY <= 150) { // Just an example
+      nav.style.backgroundColor = ' #66000000'; // or default color
+    } else {
+      nav.style.backgroundColor = 'rgba(85.5, 0, 21.6, 0.99)';
+    }
+  });
+
+
+}
+
+
 var app = document.getElementById('mytext');
 
 var typewriter = new Typewriter(app, {
